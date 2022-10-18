007a2b94f2aab30c5fccc6a3ef2fa14ff9cadb65
pratik <pratikofficial26@gmail.com>
2022-10-18 20:02:45 +0530

    correction in struct rectangle

--- src/draw.rs
+++ src/draw.rs
@@ -1,4 +1,4 @@
-use piston_window::{Rectangle,Context,G2d};
+use piston_window::{rectangle,Context,G2d};
 use piston_window::types::Color;
 
 const BLOCK_SIZE : f64 = 25.0;
@@ -11,7 +11,7 @@ pub fn draw_block(color:Color,x:i32,y:i32,con:&Context ,g:&mut G2d){
     let gui_x = to_coord(x);
     let gui_y = to_coord(y);
 
-    Rectangle(
+    rectangle(
         color,
         [gui_x,gui_y,BLOCK_SIZE,BLOCK_SIZE],
         con.transform,
@@ -30,7 +30,7 @@ pub fn draw_rectangle(
 ){
     let x=to_coord(x);
     let y=to_coord(y);
-    Rectangle(
+    rectangle(
         color,
         [
             x,
