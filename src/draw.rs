0d88c32446aaea2df0dacec7479e85c572576dcb
pratik <pratikofficial26@gmail.com>
2022-10-19 19:14:48 +0530

    snake game submission

--- src/draw.rs
+++ src/draw.rs
@@ -1,44 +1,41 @@
-use piston_window::{rectangle,Context,G2d};
+use piston_window::{rectangle, Context, G2d};
 use piston_window::types::Color;
 
-const BLOCK_SIZE : f64 = 25.0;
+const BLOCK_SIZE: f64 = 25.0;
 
-pub fn to_coord (game_coord : i32)->f64{
-    (game_coord as f64)* BLOCK_SIZE
+pub fn to_co_ordinate(game_co_ordinate: i32) -> f64 {
+	(game_co_ordinate as f64) * BLOCK_SIZE
 }
 
-pub fn draw_block(color:Color,x:i32,y:i32,con:&Context ,g:&mut G2d){
-    let gui_x = to_coord(x);
-    let gui_y = to_coord(y);
+pub fn to_co_ordinate_u32(game_co_ordinate: i32) -> u32 {
+	to_co_ordinate(game_co_ordinate) as u32
+}
 
-    rectangle(
-        color,
-        [gui_x,gui_y,BLOCK_SIZE,BLOCK_SIZE],
-        con.transform,
-        g,
-    );
+pub fn draw_block(color: Color, x: i32, y: i32, context: &Context, graphics: &mut G2d) {
+	let gui_x = to_co_ordinate(x);
+	let gui_y = to_co_ordinate(y);
+	rectangle(
+		color,
+		[gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE],
+		context.transform,
+		graphics
+	);
 }
 
 pub fn draw_rectangle(
-    color:Color,
-    x:i32,
-    y:i32,
-    width:i32,
-    height:i32,
-    con: &Context,
-    g:&mut G2d,
-){
-    let x=to_coord(x);
-    let y=to_coord(y);
-    rectangle(
-        color,
-        [
-            x,
-            y,
-            BLOCK_SIZE * (width as f64),
-            BLOCK_SIZE * (height as f64),
-        ],
-        con.transform,
-        g,
-    );
+	color: Color, x: i32, y: i32, width: i32, height: i32, context: &Context, graphics: &mut G2d
+) {
+	let x = to_co_ordinate(x);
+	let y = to_co_ordinate(y);
+	rectangle(
+		color,
+		[
+			x,
+			y,
+			BLOCK_SIZE * (width as f64),
+			BLOCK_SIZE * (height as f64)
+		],
+		context.transform,
+		graphics
+	);
 }
